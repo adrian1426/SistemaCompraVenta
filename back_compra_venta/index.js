5d970a8604ae38797539a7c6194071918f9e931a
Adrian1426 <adrianhh1426@gmail.com>
2020-07-18 00:01:32 -0500

    path y ruta estático

--- back_compra_venta/index.js
+++ back_compra_venta/index.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import path from 'path';
 
 const app = express();
+const pathPublic = path.join(__dirname, 'public')
 
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(pathPublic));
 
 app.set('port', process.env.PORT || 3001);
 
