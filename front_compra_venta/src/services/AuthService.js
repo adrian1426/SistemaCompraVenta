2b99d778f44ffc079484b6a88f6343b7d0e012a5
Adrian1426 <adrianhh1426@gmail.com>
2020-07-28 14:31:50 -0500

    AuthService, routing basado en permisos, creación de clase AuthService y sus funciones de acceso.

---
+++ front_compra_venta/src/services/AuthService.js
@@ -0,0 +1,81 @@
+import jwtDecode from 'jwt-decode';
+
+export default class AuthService {
+
+  constructor(domain) {
+    this.domain = domain;
+    this.login = this.login.bind(this);
+    this.requestFetch = this.requestFetch.bind(this);
+    this.getProfileDecode = this.getProfileDecode.bind(this);
+  }
+
+  isLoggedIn() {
+    return !!this.getToken();
+  };
+
+  setToken(token) {
+    localStorage.setItem('token', token);
+  };
+
+  getToken() {
+    return localStorage.getItem('token');
+  };
+
+  setUser(usuario) {
+    localStorage.setItem('usuario', JSON.stringify(usuario));
+  };
+
+  getUser() {
+    return JSON.parse(localStorage.getItem('usuario'));
+  };
+
+  logOut() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('usuario');
+  };
+
+  getUserRol() {
+    const user = this.getUser();
+
+    if (user) {
+      return user.rol;
+    } else {
+      return false;
+    }
+  };
+
+  async login(email, password) {
+    return this.requestFetch('/usuario/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password })
+    })
+      .then(response => {
+        this.setToken(response.token);
+        this.setUser(response.usuario);
+        return Promise.resolve(response);
+      });
+  };
+
+  async requestFetch(urlRelative, options) {
+    let headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    };
+
+    if (this.isLoggedIn()) {
+      headers['token'] = this.getToken();
+    }
+
+    return fetch(`${this.domain}${urlRelative}`, {
+      headers,
+      ...options
+    })
+      .then(response => response.json())
+      .catch(error => Promise.reject(error));
+  };
+
+  getProfileDecode() {
+    return jwtDecode(this.getToken());
+  };
+
+};
